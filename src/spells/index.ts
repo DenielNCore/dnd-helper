222e1ffd14d72329ac8e85f2f149815ca6c4eecc
Denys Khlopov <denielncore@gmail.com>
2023-12-12 20:43:01 +0200

    Deploy to gh pages

--- src/spells/index.ts
+++
@@ -1,6 +0,0 @@
-// import allSpellsDescriptions from '@/spells/description';
-// import Spell from '@/spells/list';
-//
-//
-// export { allSpellsDescriptions as spells, Spell };
-// export default { description: allSpellsDescriptions, Spell: Spell.Spell };
--- src/types/spellCard.ts
+++
@@ -1,9 +0,0 @@
-enum CardSize {
-  Full = 0,
-  Row = 1,
-  Icon = 2,
-}
-
-export default { CardSize };
-
-export { CardSize };
